7d4f0fc9c6105c3b5f2914398afd880e34d01c51
99006297 <89571674+99006297@users.noreply.github.com>
2021-09-16 13:16:43 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,5 +1,16 @@
 #ifndef activity3_H_INCLUDED
 #define activity3_H_INCLUDED
+/**
+ * @file main.c
+ * @author Thapaswi bolisettty
+ * @brief Embedded C project
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 
 void config_timer(void);
 void pwm_output(void);
