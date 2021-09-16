6f53076a4fd78e6322bd11d0d36004100661807b
99006297 <89571674+99006297@users.noreply.github.com>
2021-09-16 13:16:08 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,5 +1,16 @@
 #ifndef activity2_H_INCLUDED
 #define activity2_H_INCLUDED
+/**
+ * @file main.c
+ * @author Thapaswi bolisettty
+ * @brief Embedded C project
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 
 void InitADC();
 uint16_t ReadADC(uint8_t ch);
