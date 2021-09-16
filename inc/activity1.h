7ef820cc0a89d045c830ed95499634207cff50a6
99006297 <89571674+99006297@users.noreply.github.com>
2021-09-16 13:15:51 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,5 +1,16 @@
 #ifndef activity1_H_INCLUDED
 #define activity1_H_INCLUDED
+/**
+ * @file main.c
+ * @author Thapaswi bolisettty
+ * @brief Embedded C project
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 
 #define LED_PORT (PORTB)
 #define IN_PIN1 (PORTB6)
